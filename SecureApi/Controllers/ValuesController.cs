c5f3aa27a15a2449820e249dbc372dacb26fbf7b
Tyrone Davison <t.p.davison@tees.ac.uk>
2021-11-21 11:11:00 +0000

    Add initial files

---
+++ SecureApi/Controllers/ValuesController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using SecureApi.Models;
+
+namespace SecureApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var rand = new Random().Next();
+            var value = new GetValuesDto
+            {
+                Number = rand,
+                Message = $"Hello, your result is {rand}."
+            };
+            return Ok(value);
+        }
+    }
+}
---
+++ SecureApi/Models/GetValuesDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace SecureApi.Models
+{
+    public class GetValuesDto
+    {
+        public int Number { get; set; }
+        public string Message { get; set; }
+    }
+}
